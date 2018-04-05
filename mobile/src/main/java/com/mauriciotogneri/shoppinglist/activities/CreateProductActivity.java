e7f453b5aec3c094a40d1ca1ca42c0810e912209
mtogneri <mtogneri@openwt.com>
2018-04-05 22:57:58 +0200

    Added image placeholder

--- mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/CreateProductActivity.java
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/CreateProductActivity.java
@@ -3,9 +3,11 @@ package com.mauriciotogneri.shoppinglist.activities;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.mauriciotogneri.shoppinglist.R;
 import com.mauriciotogneri.shoppinglist.base.BaseActivity;
 import com.mauriciotogneri.shoppinglist.views.CreateProductView;
 import com.mauriciotogneri.shoppinglist.views.CreateProductView.CreateProductViewObserver;
+import com.mauriciotogneri.shoppinglist.views.Dialogs;
 
 public class CreateProductActivity extends BaseActivity<CreateProductView> implements CreateProductViewObserver
 {
@@ -26,6 +28,46 @@ public class CreateProductActivity extends BaseActivity<CreateProductView> imple
     {
         Intent intent = new Intent(this, ManageCategoriesActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onChangeImage()
+    {
+        String[] options = new String[3];
+        options[0] = getString(R.string.label_source_camera);
+        options[1] = getString(R.string.label_source_gallery);
+        options[2] = "Search";
+
+        Dialogs dialogs = new Dialogs(this);
+        dialogs.options(getString(R.string.label_select_picture_source), options, option -> {
+            if (option == 0)
+            {
+                imageFromCamera();
+            }
+            else if (option == 1)
+            {
+                imageFromGallery();
+            }
+            else if (option == 2)
+            {
+                imageFromSearch();
+            }
+        });
+    }
+
+    private void imageFromCamera()
+    {
+        Toast.makeText(this, "CAMERA", Toast.LENGTH_SHORT).show();
+    }
+
+    private void imageFromGallery()
+    {
+        Toast.makeText(this, "GALLERY", Toast.LENGTH_SHORT).show();
+    }
+
+    private void imageFromSearch()
+    {
+        Toast.makeText(this, "SEARCH", Toast.LENGTH_SHORT).show();
     }
 
     @Override
