a7e32f007c1e86aed3194d486cd97e6d2274ee93
mtogneri <mtogneri@openwt.com>
2018-04-07 23:21:43 +0200

    Loading image from gallery

--- mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/CreateProductActivity.java
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/CreateProductActivity.java
@@ -1,18 +1,32 @@
 package com.mauriciotogneri.shoppinglist.activities;
 
+import android.Manifest.permission;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.widget.Toast;
 
+import com.mauriciotogneri.androidutils.intents.Intents;
+import com.mauriciotogneri.androidutils.permissions.OnPermissionGranted;
+import com.mauriciotogneri.androidutils.permissions.Permissions;
+import com.mauriciotogneri.androidutils.permissions.PermissionsResult;
 import com.mauriciotogneri.shoppinglist.R;
 import com.mauriciotogneri.shoppinglist.base.BaseActivity;
+import com.mauriciotogneri.shoppinglist.utils.ResoursesUtils;
 import com.mauriciotogneri.shoppinglist.views.CreateProductView;
 import com.mauriciotogneri.shoppinglist.views.CreateProductView.CreateProductViewObserver;
 import com.mauriciotogneri.shoppinglist.views.Dialogs;
 
+import java.io.File;
+
 public class CreateProductActivity extends BaseActivity<CreateProductView> implements CreateProductViewObserver
 {
-    private static final int SEARCH_IMAGE_REQUEST_CODE = 1000;
+    private static final int READ_DISK_PERMISSION = 1001;
+
+    private static final int CAMERA_IMAGE_REQUEST_CODE = 2001;
+    private static final int GALLERY_IMAGE_REQUEST_CODE = 2002;
+    private static final int SEARCH_IMAGE_REQUEST_CODE = 2003;
 
     @Override
     protected void initialize()
@@ -65,7 +79,16 @@ public class CreateProductActivity extends BaseActivity<CreateProductView> imple
 
     private void imageFromGallery()
     {
-        Toast.makeText(this, "GALLERY", Toast.LENGTH_SHORT).show();
+        Permissions permissions = new Permissions(this, this);
+        permissions.request(READ_DISK_PERMISSION, permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @OnPermissionGranted(requestCode = READ_DISK_PERMISSION, permission = permission.READ_EXTERNAL_STORAGE)
+    public void onPermissionsReadStorageGranted()
+    {
+        Intents.selectFromGallery("image/*")
+                .requestCode(GALLERY_IMAGE_REQUEST_CODE)
+                .send(this);
     }
 
     private void imageFromSearch()
@@ -80,16 +103,56 @@ public class CreateProductActivity extends BaseActivity<CreateProductView> imple
         Toast.makeText(this, "ACTION", Toast.LENGTH_SHORT).show();
     }
 
+    private void processCameraImage(Uri uri)
+    {
+        // TODO
+    }
+
+    private void processGalleryImage(Uri uri)
+    {
+        try
+        {
+            File file = ResoursesUtils.fileFromUri(this, uri);
+            view.image(file.getAbsolutePath());
+        }
+        catch (Exception e)
+        {
+            // TODO
+            Toast.makeText(this, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void processSearchImage(String imageUrl)
+    {
+        view.image(imageUrl);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if ((requestCode == SEARCH_IMAGE_REQUEST_CODE) && (resultCode == Activity.RESULT_OK))
+        if ((requestCode == CAMERA_IMAGE_REQUEST_CODE) && (resultCode == Activity.RESULT_OK))
         {
-            String imageUrl = data.getStringExtra(SearchImageActivity.PARAM_IMAGE_URL);
-            view.image(imageUrl);
+            processCameraImage(data.getData());
         }
+        else if ((requestCode == GALLERY_IMAGE_REQUEST_CODE) && (resultCode == Activity.RESULT_OK))
+        {
+            processGalleryImage(data.getData());
+        }
+        else if ((requestCode == SEARCH_IMAGE_REQUEST_CODE) && (resultCode == Activity.RESULT_OK))
+        {
+            processSearchImage(data.getStringExtra(SearchImageActivity.PARAM_IMAGE_URL));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        PermissionsResult permissionsResult = new PermissionsResult(this);
+        permissionsResult.process(requestCode, permissions, grantResults);
     }
 
     @Override
