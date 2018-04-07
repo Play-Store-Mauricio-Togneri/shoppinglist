f61020234dd387a9ea55cafd8332f27356a6c7ec
mtogneri <mtogneri@openwt.com>
2018-04-07 22:06:41 +0200

    Finished search image screen

---
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/adapters/SearchImageAdapter.java
@@ -0,0 +1,82 @@
+package com.mauriciotogneri.shoppinglist.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+
+import com.bumptech.glide.Glide;
+import com.mauriciotogneri.androidutils.Screen;
+import com.mauriciotogneri.androidutils.Screen.ScreenSize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchImageAdapter extends BaseAdapter
+{
+    private final Context context;
+    private final List<String> images;
+
+    public SearchImageAdapter(Context context)
+    {
+        this.context = context;
+        this.images = new ArrayList<>();
+    }
+
+    public void loadImages(List<String> list)
+    {
+        images.clear();
+        images.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount()
+    {
+        return images.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return images.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return images.get(position).hashCode();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ImageView imageView;
+
+        if (convertView == null)
+        {
+            ScreenSize screenSize = new Screen(context).size();
+
+            int size = screenSize.width() / 3;
+            int padding = (int) ((float) size / 20f);
+
+            imageView = new ImageView(context);
+            imageView.setScaleType(ScaleType.CENTER_INSIDE);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(size, size));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(padding, padding, padding, padding);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+
+        Glide.with(context)
+                .load(images.get(position))
+                .into(imageView);
+
+        return imageView;
+    }
+}
---
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/database/LoadImages.java
@@ -0,0 +1,72 @@
+package com.mauriciotogneri.shoppinglist.database;
+
+import android.os.AsyncTask;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class LoadImages extends AsyncTask<String, Void, List<String>>
+{
+    private final OkHttpClient client;
+    private final OnImagesLoaded callback;
+
+    private static final String URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?mkt=en-us&q=";
+
+    public LoadImages(OnImagesLoaded callback)
+    {
+        this.client = new OkHttpClient();
+        this.callback = callback;
+    }
+
+    @Override
+    protected List<String> doInBackground(String... queries)
+    {
+        Request request = new Request.Builder()
+                .url(URL + queries[0])
+                .header("Ocp-Apim-Subscription-Key", "c1487859adda4a8a8304c6f42f4e3be6")
+                .build();
+
+        try
+        {
+            Response response = client.newCall(request).execute();
+            JsonElement element = new JsonParser().parse(response.body().string());
+            JsonArray list = element.getAsJsonObject().getAsJsonArray("value");
+
+            List<String> result = new ArrayList<>();
+
+            for (int i = 0; i < list.size(); i++)
+            {
+                JsonObject object = list.get(i).getAsJsonObject();
+
+                result.add(object.get("contentUrl").getAsString());
+            }
+
+            return result;
+        }
+        catch (Exception e)
+        {
+            return Arrays.asList();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(List<String> images)
+    {
+        callback.onImagesLoaded(images);
+    }
+
+    public interface OnImagesLoaded
+    {
+        void onImagesLoaded(List<String> images);
+    }
+}
