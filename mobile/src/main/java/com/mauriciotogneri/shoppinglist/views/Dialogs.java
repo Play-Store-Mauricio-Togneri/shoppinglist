2c31bf9c59175235bca186da7ebda5ec75d352ed
mtogneri <mtogneri@openwt.com>
2018-04-08 13:25:22 +0200

    Added dialog to input text

--- mobile/src/main/java/com/mauriciotogneri/shoppinglist/views/Dialogs.java
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/views/Dialogs.java
@@ -3,9 +3,17 @@ package com.mauriciotogneri.shoppinglist.views;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.text.Editable;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import com.mauriciotogneri.shoppinglist.R;
 
@@ -36,6 +44,71 @@ public class Dialogs
         builder.show();
     }
 
+    public void input(Context context, String title, String initialInput, OnInputConfirmed callback)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.view_dialog_input, null);
+        EditText nameField = view.findViewById(R.id.input);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title(title));
+        builder.setView(view);
+        builder.setPositiveButton(R.string.button_accept, (dialogInterface, i) -> callback.onInputConfirmed(nameField.getText().toString()));
+        builder.setNegativeButton(R.string.button_cancel, null);
+
+        AlertDialog dialog = builder.create();
+        Window window = dialog.getWindow();
+
+        if (window != null)
+        {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+
+        dialog.show();
+
+        nameField.setText(initialInput);
+        nameField.requestFocus();
+        nameField.selectAll();
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(initialInput.length() != 0);
+
+        nameField.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+            }
+
+            @Override
+            public void afterTextChanged(Editable text)
+            {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(text.length() != 0);
+            }
+        });
+
+        nameField.setOnEditorActionListener((view1, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE)
+            {
+                try
+                {
+                    dialog.dismiss();
+                }
+                catch (Exception e)
+                {
+                    // ignore
+                }
+
+                callback.onInputConfirmed(nameField.getText().toString());
+            }
+
+            return false;
+        });
+    }
+
     private SpannableStringBuilder title(String text)
     {
         SpannableStringBuilder spannable = new SpannableStringBuilder(text);
@@ -57,5 +130,10 @@ public class Dialogs
     public interface OnConfirmationAccepted
     {
         void onConfirmationAccepted();
+    }
+
+    public interface OnInputConfirmed
+    {
+        void onInputConfirmed(String input);
     }
 }
