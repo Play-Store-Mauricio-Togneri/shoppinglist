0fc0c967cf59152949658d9e17764066b0ef774c
mtogneri <mtogneri@openwt.com>
2018-04-08 11:29:42 +0200

    Improved screen to manage categories

---
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/adapters/CategoryAdapter.java
@@ -0,0 +1,42 @@
+package com.mauriciotogneri.shoppinglist.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mauriciotogneri.androidutils.uibinder.annotations.BindView;
+import com.mauriciotogneri.shoppinglist.R;
+import com.mauriciotogneri.shoppinglist.adapters.CategoryAdapter.ViewHolder;
+import com.mauriciotogneri.shoppinglist.base.BaseListAdapter;
+import com.mauriciotogneri.shoppinglist.base.BaseViewHolder;
+
+public class CategoryAdapter extends BaseListAdapter<String, ViewHolder>
+{
+    public CategoryAdapter(Context context)
+    {
+        super(context, R.layout.item_category);
+    }
+
+    @Override
+    protected void fillView(ViewHolder viewHolder, String category, int position)
+    {
+        viewHolder.name.setText(category);
+    }
+
+    @Override
+    protected ViewHolder viewHolder(View view)
+    {
+        return new ViewHolder(view);
+    }
+
+    public static class ViewHolder extends BaseViewHolder
+    {
+        @BindView(R.id.category_name)
+        public TextView name;
+
+        protected ViewHolder(View view)
+        {
+            super(view);
+        }
+    }
+}
