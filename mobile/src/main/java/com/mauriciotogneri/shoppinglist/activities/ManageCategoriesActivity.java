038d12bdaa09affb4f2b685dcd72ba8d3a828010
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-04-08 13:25:22 +0200

    Added dialog to input text

--- mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/ManageCategoriesActivity.java
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/activities/ManageCategoriesActivity.java
@@ -1,8 +1,12 @@
 package com.mauriciotogneri.shoppinglist.activities;
 
+import android.widget.Toast;
+
+import com.mauriciotogneri.shoppinglist.R;
 import com.mauriciotogneri.shoppinglist.base.BaseActivity;
 import com.mauriciotogneri.shoppinglist.database.LoadCategories;
 import com.mauriciotogneri.shoppinglist.database.LoadCategories.OnCategoriesLoaded;
+import com.mauriciotogneri.shoppinglist.views.Dialogs;
 import com.mauriciotogneri.shoppinglist.views.ManageCategoriesView;
 import com.mauriciotogneri.shoppinglist.views.ManageCategoriesView.ManageCategoriesViewObserver;
 
@@ -26,13 +30,49 @@ public class ManageCategoriesActivity extends BaseActivity<ManageCategoriesView>
     @Override
     public void onCategorySelected(String category)
     {
-        // TODO
+        String[] options = new String[2];
+        options[0] = "Rename"; // TODO
+        options[1] = getString(R.string.button_remove);
+
+        Dialogs dialogs = new Dialogs(this);
+        dialogs.options(category, options, option -> {
+            if (option == 0)
+            {
+                renameCategory(category);
+            }
+            else if (option == 1)
+            {
+                confirmRemoveCategory(category);
+            }
+        });
+    }
+
+    private void renameCategory(String category)
+    {
+        Dialogs dialogs = new Dialogs(this);
+        dialogs.input(this, getString(R.string.label_product_new_category), category, input -> {
+            Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void confirmRemoveCategory(String category)
+    {
+        Dialogs dialogs = new Dialogs(this);
+        dialogs.confirmation(category, getString(R.string.confirmation_remove_category), () -> removeCategory(category));
+    }
+
+    private void removeCategory(String category)
+    {
+
     }
 
     @Override
     public void onAddCategory()
     {
-        // TODO
+        Dialogs dialogs = new Dialogs(this);
+        dialogs.input(this, getString(R.string.label_product_new_category), "", input -> {
+            Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        });
     }
 
     @Override
