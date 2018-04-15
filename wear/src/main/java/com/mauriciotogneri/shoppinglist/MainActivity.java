826fc7d29a45e452c2585b8f6e5b4286d84d4bd2
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-04-15 22:33:39 +0200

    Making work test messaging

--- wear/src/main/java/com/mauriciotogneri/shoppinglist/MainActivity.java
+++ wear/src/main/java/com/mauriciotogneri/shoppinglist/MainActivity.java
@@ -1,9 +1,9 @@
-package com.mauriciotogneri.shoppinglist.activities;
+package com.mauriciotogneri.shoppinglist;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.wearable.view.WatchViewStub;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
@@ -11,30 +11,32 @@ import com.google.android.gms.wearable.MessageClient;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
-import com.mauriciotogneri.shoppinglist.R;
 
 import java.util.List;
 
-public class TestActivity extends Activity implements MessageClient.OnMessageReceivedListener
+public class MainActivity extends Activity implements MessageClient.OnMessageReceivedListener
+
 {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_main);
 
-        setContentView(R.layout.screen_test);
-
-        findViewById(R.id.test).setOnClickListener(v -> sendMessage());
+        WatchViewStub stub = findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(stub1 -> {
+            stub1.findViewById(R.id.test).setOnClickListener(v -> test());
+        });
     }
 
-    private void sendMessage()
+    private void test()
     {
         Task<List<Node>> task = Wearable.getNodeClient(this).getConnectedNodes();
         task.addOnSuccessListener(nodes ->
         {
             for (Node node : nodes)
             {
-                Wearable.getMessageClient(this).sendMessage(node.getId(), "aaa", new byte[] {1, 2, 3});
+                Wearable.getMessageClient(this).sendMessage(node.getId(), "bbb", new byte[] {1, 2, 3, 4, 5});
             }
         });
     }
@@ -58,6 +60,6 @@ public class TestActivity extends Activity implements MessageClient.OnMessageRec
     @Override
     public void onMessageReceived(@NonNull MessageEvent messageEvent)
     {
-        Toast.makeText(this, messageEvent.getPath() + messageEvent.getData().length, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, messageEvent.getPath() +  messageEvent.getData().length, Toast.LENGTH_SHORT).show();
     }
 }
