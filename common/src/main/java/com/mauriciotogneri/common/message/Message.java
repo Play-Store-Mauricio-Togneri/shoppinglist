399c677ac6c30a7898f2e084b02ad2d8c89ebcde
mtogneri <mtogneri@openwt.com>
2018-04-15 23:15:22 +0200

    Improved message handling

--- common/src/main/java/com/mauriciotogneri/common/message/Message.java
+++ common/src/main/java/com/mauriciotogneri/common/message/Message.java
@@ -1,35 +1,52 @@
 package com.mauriciotogneri.common.message;
 
+import android.content.Context;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
+import java.nio.charset.Charset;
+
 public class Message
 {
     private final String nodeId;
-    private final String path;
-    private final byte[] payload;
+    private final String action;
+    private final String payload;
 
-    public Message(String nodeId, String path, byte[] payload)
+    public static final String REQUEST_PRODUCTS = "request.products";
+    public static final String RESPONSE_PRODUCTS = "response.products";
+
+    public Message(String nodeId, String action, String payload)
     {
         this.nodeId = nodeId;
-        this.path = path;
+        this.action = action;
         this.payload = payload;
     }
 
-    public Message(String nodeId, String path)
+    public Message(MessageEvent messageEvent)
     {
-        this(nodeId, path, null);
+        this.nodeId = messageEvent.getSourceNodeId();
+        this.action = messageEvent.getPath();
+        this.payload = new String(messageEvent.getData(), Charset.forName("UTF-8"));
     }
 
-    public String getNodeId()
+    public String nodeId()
     {
         return nodeId;
     }
 
-    public String getPath()
+    public String action()
     {
-        return path;
+        return action;
     }
 
-    public byte[] getPayload()
+    public String payload()
     {
         return payload;
+    }
+
+    public void send(Context context)
+    {
+        Wearable.getMessageClient(context).sendMessage(nodeId, action, payload.getBytes(Charset.forName("utf-8")));
     }
 }
--- mobile/src/main/java/com/mauriciotogneri/shoppinglist/utils/WearableService.java
+++ mobile/src/main/java/com/mauriciotogneri/shoppinglist/utils/WearableService.java
@@ -1,7 +1,48 @@
 package com.mauriciotogneri.shoppinglist.utils;
 
-import com.google.android.gms.wearable.WearableListenerService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
-public class WearableService extends WearableListenerService
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.mauriciotogneri.common.message.Message;
+
+public class WearableService extends Service implements MessageClient.OnMessageReceivedListener
 {
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        Wearable.getMessageClient(this).addListener(this);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull MessageEvent messageEvent)
+    {
+        Message message = new Message(messageEvent);
+
+        if (message.action().equals(Message.REQUEST_PRODUCTS))
+        {
+            Message response = new Message(message.nodeId(), Message.RESPONSE_PRODUCTS, "THIS ARE THE PRODUCTS");
+            response.send(this);
+        }
+    }
 }
